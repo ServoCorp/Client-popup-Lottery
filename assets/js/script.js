148b83e7ae1fdd6d94db773b71f9827faf1dcc08
ServoCorp <80141504+ServoCorp@users.noreply.github.com>
2021-10-14 09:49:45 -0500

    started JS functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,22 @@
-const popupWait = 5;
+const scratch = document.getElementsByClassName("topLayer");
+myCanvas = scratch.getContext("2d");
+scratchRadius = (scratch.width / 100) * 5;
+img = new Image();
 
-function popUp() {
+if (scratchRadius < 50) {scratchRadius = 50};
 
+img.onload = function () {
+    myCanvas.drawImage(img, 0, 0, scratch.width, scratch.height);
+}
+
+img.loc = "";
+
+function detectLeftButton(event) {
+    if ('buttons' in event) {
+        return event.buttons === 1;
+    } else if ('which' in event) {
+        return event.which === 1;
+    } else {
+        return event.button === 1;
+    }
 }
