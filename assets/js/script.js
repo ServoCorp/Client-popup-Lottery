7a3a4a859c31c8858576ccca05958504fda57b8b
ServoCorp <80141504+ServoCorp@users.noreply.github.com>
2021-10-15 12:23:13 -0500

    added scratch function. rough prototype done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,21 @@
-const scratch = document.getElementsByClassName("topLayer");
-myCanvas = scratch.getContext("2d");
-scratchRadius = (scratch.width / 100) * 5;
+var bridge = document.getElementById("topLayer"),
+bridgeCanvas = bridge.getContext('2d'),
+brushRadius = (bridge.width / 100) * 5,
 img = new Image();
 
-if (scratchRadius < 50) {scratchRadius = 50};
+if (brushRadius < 10) { brushRadius = 10 }
 
-img.onload = function () {
-    myCanvas.drawImage(img, 0, 0, scratch.width, scratch.height);
+img.onload = function(){  
+	bridgeCanvas.drawImage(img, 0, 0, topLayer.width, topLayer.height);
 }
-
-img.loc = "";
+img.loc = './assets/img/'; // this is the location to the file for top scratch off image
+img.filename = 'topLayer.png'; // CHANGE NAME OF FILE HERE. THIS IS THE TOP OVERLAY (SCRATCH OFF IMAGE)
+if (window.devicePixelRatio >= 2) {
+	var nameParts = img.filename.split('.');
+	img.src = img.loc + nameParts[0]+"-2x"+"."+nameParts[1];
+} else {
+	img.src = img.loc + img.filename;
+}
 
 function detectLeftButton(event) {
     if ('buttons' in event) {
@@ -20,3 +26,36 @@ function detectLeftButton(event) {
         return event.button === 1;
     }
 }
+
+function getBrushPos(xRef, yRef) {
+	var bridgeRect = topLayer.getBoundingClientRect();
+    return {
+	  x: Math.floor((xRef-bridgeRect.left)/(bridgeRect.right-bridgeRect.left)*topLayer.width),
+	  y: Math.floor((yRef-bridgeRect.top)/(bridgeRect.bottom-bridgeRect.top)*topLayer.height)
+    };
+}
+      
+function drawDot(mouseX,mouseY){
+	bridgeCanvas.beginPath();
+    bridgeCanvas.arc(mouseX, mouseY, brushRadius, 0, 2*Math.PI, true);
+    bridgeCanvas.fillStyle = '#000';
+    bridgeCanvas.globalCompositeOperation = "destination-out";
+    bridgeCanvas.fill();
+}
+
+topLayer.addEventListener("mousemove", function(e) {
+	var brushPos = getBrushPos(e.clientX, e.clientY);
+  var leftBut = detectLeftButton(e);
+  if (leftBut == 1) {
+		drawDot(brushPos.x, brushPos.y);
+  }
+}, false);
+
+topLayer.addEventListener("touchmove", function(e) {
+    e.preventDefault();
+    var touch = e.targetTouches[0];
+    if (touch) {
+    var brushPos = getBrushPos(touch.pageX, touch.pageY);
+        drawDot(brushPos.x, brushPos.y);
+    }
+}, false);
