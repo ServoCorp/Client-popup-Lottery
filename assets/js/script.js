11d9e463272f2bc77c471897b3b22c4438cd915e
ServoCorp <80141504+ServoCorp@users.noreply.github.com>
2021-10-25 21:40:14 -0500

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,7 +16,7 @@ img.loc = './assets/img/'; // this is the location to the file for top scratch o
 img.filename = 'topLayer.png'; // CHANGE NAME OF FILE HERE. THIS IS THE TOP OVERLAY (SCRATCH OFF IMAGE)
 if (window.devicePixelRatio >= 2) {
 	var nameParts = img.filename.split('.');
-	img.src = img.loc + nameParts[0]+""+"."+nameParts[1];
+	img.src = img.loc + nameParts[0]+""+"."+nameParts[1]; //dont need new image rn so lazy workaround
 } else {
 	img.src = img.loc + img.filename;
 }
